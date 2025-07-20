acdd536805a5d5220f8475b13bfe3b15a0ba5bcf
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-20 10:55:10 +0300

    List all students and update a student done!

--- StudentManagementSystem/src/main/java/com/spring/boot/studentmanagementsystem/Controller/StudentController.java
+++ StudentManagementSystem/src/main/java/com/spring/boot/studentmanagementsystem/Controller/StudentController.java
@@ -2,30 +2,51 @@ package com.spring.boot.studentmanagementsystem.Controller;
 
 import com.spring.boot.studentmanagementsystem.Api.ApiResponse;
 import com.spring.boot.studentmanagementsystem.Model.Student;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/v1/student")
 public class StudentController {
-//    Q1 : Create Spring program with request on the following endpoints :
+    //    Q1 : Create Spring program with request on the following endpoints :
 //    Create a controller called studentController with the following endpoints.
     ArrayList<Student> students = new ArrayList<>();
     int idCounter = 0;
-//• Create a new student (ID, ) -- post
+
     @PostMapping("/new")
-    public ApiResponse createStudent(@RequestBody Student student){
-        students.add(student);
+    public ApiResponse createStudent(@RequestBody Student student) {
         idCounter++;
+        student.setID(Integer.toString(idCounter));
+        students.add(student);
         return new ApiResponse("Student was created successfully!", "200 OK");
     }
 
-//• Display all students.
-//• Update a student
+    @GetMapping("/list")
+    public ArrayList<Student> displayStudents() {
+        return students;
+    }
+
+    //• Update a student
+    @PutMapping("/update/{iD}")
+    public ApiResponse updateStudent(@PathVariable String iD, @RequestBody Student student) {
+        boolean updated = false;
+
+        for (Student s : students) {
+            if (s.getID().equals(iD)) {
+                s.setName(student.getName());
+                s.setAge(student.getAge());
+                s.setDegree(student.getDegree());
+                s.setGPA(student.getGPA());
+                updated = true;
+            }
+        }
+        if (updated) {
+            return new ApiResponse("Student with id: was successfully updated", "200 OK");
+        } else {
+            return new ApiResponse("Error, the student "+student.getName()+" with id: "+student.getID()+" does not exist","404 Not found")
+        }
+    }
 //• Delete a student
 //• Based on GPA, classify students into honors categories.
 //• Display a group of students whose GPA is greater than the average
