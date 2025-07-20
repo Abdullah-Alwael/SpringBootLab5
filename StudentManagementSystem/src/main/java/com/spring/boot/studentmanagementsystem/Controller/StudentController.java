372bab28f9dd30f4ec0f19ed3d39f0a2efb06beb
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-20 10:20:35 +0300

    Setting up

--- StudentManagementSystem/src/main/java/com/spring/boot/studentmanagementsystem/Controller/StudentController.java
+++ StudentManagementSystem/src/main/java/com/spring/boot/studentmanagementsystem/Controller/StudentController.java
@@ -1,4 +1,14 @@
 package com.spring.boot.studentmanagementsystem.Controller;
 
 public class StudentController {
+//    Q1 : Create Spring program with request on the following endpoints :
+//    Create a controller called studentController with the following endpoints.
+
+//• Create a new student (ID, name, age, degree, GPA)
+//• Display all students.
+//• Update a student
+//• Delete a student
+//• Based on GPA, classify students into honors categories.
+//• Display a group of students whose GPA is greater than the average
+//    GPA.
 }
--- StudentManagementSystem/src/main/java/com/spring/boot/studentmanagementsystem/Model/Student.java
+++ StudentManagementSystem/src/main/java/com/spring/boot/studentmanagementsystem/Model/Student.java
@@ -1,4 +1,13 @@
 package com.spring.boot.studentmanagementsystem.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Student {
+    private String iD, name;
+    private int age;
+    private double degree, gPA;
+
 }
